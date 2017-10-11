7e17270ec02184258759c5f92450edbc03169ef6
Buck Baskin <mobile.wbaskin@gmail.com>
2017-10-10 23:37:30 -0400

    Trace the number of bytecode instructions in user code (issues with C calls)

--- bigo/__init__.py
+++ bigo/__init__.py
@@ -1,4 +1,9 @@
+import dis
+import sys
+
 from datetime import datetime
+from io import StringIO
+from time import process_time
 
 runtime_db = {}
 
@@ -11,19 +16,91 @@ def save_runtime(func_name, size, total_millis):
     print('saving %s, %d, %f' % (func_name, size, total_millis,))
     runtime_db[func_name][size].append(total_millis)
 
+class AbstractTracer(object):
+    def __init__(self, func):
+        raise NotImplementedError()
+
+    def start(self):
+        raise NotImplementedError()
+
+    def end(self):
+        raise NotImplementedError()
+
+    def runtime(self):
+        raise NotImplementedError()
+
+
+class CPUTracer(AbstractTracer):
+    def __init__(self, func):
+        self.accum = 0
+        self.__last_accum = self.accum
+
+    def start(self):
+        self.accum = 0
+        sys.settrace(self._tracer)
+
+    def end(self):
+        sys.settrace(None)
+        self.__last_accum = self.accum
+
+    def runtime(self):
+         return self.__last_accum
+    
+    def _tracer(self, frame, event, arg):
+        with StringIO() as out:
+            dis.dis(frame.f_code.co_code, file=out)
+            # print(len(out.getvalue().split('\n')))
+            self.accum += len(out.getvalue().split('\n'))
+
+        # sys.exit(1)
+        if event == 'return':
+            print('%5d trace of %s, %s, arg' % (self.accum, frame, event,))
+        else:
+            print('%5d trace of %s, %s, %s' % (self.accum, frame, event, arg,))
+        return self._tracer
+
+class AbstractTimeTracer(AbstractTracer):
+    def __init__(self, func):
+        self.time = None
+        self.start_time = None
+        self.end_time = None
+
+    def start(self):
+        self.start_time = self.time()
+
+    def end(self):
+        self.end_time = self.time()
+
+    def runtime(self):
+        try:
+            return (self.end_time - self.start_time).total_seconds() * 1000
+        except AttributeError:
+            return (self.end_time - self.start_time) * 1000
+
+class ProcessTimeTracer(AbstractTimeTracer):
+    def __init__(self, func):
+        super(ProcessTimeTracer, self).__init__(func)
+        self.time = process_time
+
+class TotalTimeTracer(AbstractTimeTracer):
+    def __init__(self, func):
+        super(TotalTimeTracer, self).__init__(func)
+        self.time = datetime.now
+        
+
 def analyze(input_sizer, measure='time'):
-    print('analyze a function!')
+    if measure=='cpu':
+        Tracker = CPUTracer
+    else:
+        Tracker = ProcessTimeTracer
     def decorator(func):
-        print('decorate func %s' % (func.__name__,))
+        tracker = Tracker(func)
         def special_func(*args, **kwargs):
-            print('start time')
-            start = datetime.now()
+            tracker.start()
             value = func(*args, **kwargs)
-            end = datetime.now()
-            runtime = (end - start).total_seconds() * 1000
+            tracker.end()
             save_runtime(func.__name__, input_sizer((args, kwargs,)),
-            runtime)
-            print('end time')
+                tracker.runtime())
             return value
         special_func.__name__ = func.__name__
         return special_func
--- tests/example.py
+++ tests/example.py
@@ -3,13 +3,17 @@ from random import sample
 
 # inputs: tuple with (args, kwargs)
 @a(lambda inputs: len(inputs[0][0]), measure='time')
-def sort_me(list_):
-    print('list_, %d' % len(list_))
+def sort_me1(list_):
+    return sorted(list_)
+
+@a(lambda inputs: len(inputs[0][0]), measure='cpu')
+def sort_me2(list_):
     return sorted(list_)
 
 def example_problem(size):
     return sample(list(range(0, size)), k=int(size))
 
 for i in range(0, 6):
-    sort_me(example_problem(10**i))
+    sort_me1(example_problem(10**i))
+    sort_me2(example_problem(10**i))
 
